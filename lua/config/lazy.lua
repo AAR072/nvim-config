c258067e7f64cefd8a274b22e19f162e01f91dd6
Ali-Akber Raza <aaoblivion@gmail.com>
2024-10-06 20:36:45 -0400

    Setup tab

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -118,12 +118,24 @@ require('lspconfig').lua_ls.setup({
 require("typescript-tools").setup({
 })
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
   },
-  mapping = cmp.mapping.preset.insert({}),
+  mapping = cmp.mapping.preset.insert({
+-- Navigate between completion items
+    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  }),
   snippet = {
     expand = function(args)
       vim.snippet.expand(args.body)
