1051839b0974191c1b7528588169c81466e5bdc0
Ali-Akber Raza <aaoblivion@gmail.com>
2024-10-07 11:17:12 -0400

    added indenting

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
 require("config.lazy")
 require("config.remap")
 vim.opt.relativenumber = true
+-- Enable filetype detection, plugin and indent support
+vim.cmd('filetype plugin indent on')
+
+-- Set expandtab to use spaces instead of tabs
+vim.opt.expandtab = true
+
+-- Show existing tab with 2 spaces width
+vim.opt.tabstop = 2
+
+-- Set softtabstop to 2 spaces
+vim.opt.softtabstop = 2
+
+-- When indenting with '>', use 2 spaces width
+vim.opt.shiftwidth = 2
+
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,92 +1,92 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 require("lazy").setup({
-	spec = {
-		{'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-		{'neovim/nvim-lspconfig'},
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/nvim-cmp'},
-		{"pmizio/typescript-tools.nvim"},
-		{
-			"jiaoshijie/undotree",
-			dependencies = "nvim-lua/plenary.nvim",
-			config = true,
-			keys = { -- load the plugin only when using it's keybinding:
-				{ "<leader><F5>", "<cmd>lua require('undotree').toggle()<cr>" },
-			},
-		},
-		{
-			"kdheepak/lazygit.nvim",
-			lazy = true,
-			cmd = {
-				"LazyGit",
-				"LazyGitConfig",
-				"LazyGitCurrentFile",
-				"LazyGitFilter",
-				"LazyGitFilterCurrentFile",
-			},
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-			},
-			keys = {
-				{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-			}
-		},
-		{
-			'windwp/nvim-autopairs',
-			event = "InsertEnter",
-			config = true
-		},
-		{'nvim-telescope/telescope.nvim', tag = '0.1.8'},
-		{
-			'sainnhe/gruvbox-material',
-			lazy = false,
-			priority = 1000,
-			config = function()
-				-- Optionally configure and load the colorscheme
-				-- directly inside the plugin declaration.
-				vim.g.gruvbox_material_enable_italic = true
-				vim.g.gruvbox_material_enable_bold = true
-				vim.g.gruvbox_material_enable_bold = true
-				vim.cmd.colorscheme('gruvbox-material')
-			end
-		},
-		{'nvim-telescope/telescope.nvim', tag = '0.1.8'},
-		{
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			config = function () 
-				local configs = require("nvim-treesitter.configs")
+  spec = {
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {"pmizio/typescript-tools.nvim"},
+    {
+      "jiaoshijie/undotree",
+      dependencies = "nvim-lua/plenary.nvim",
+      config = true,
+      keys = { -- load the plugin only when using it's keybinding:
+        { "<leader><F5>", "<cmd>lua require('undotree').toggle()<cr>" },
+      },
+    },
+    {
+      "kdheepak/lazygit.nvim",
+      lazy = true,
+      cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+      keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+      }
+    },
+    {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      config = true
+    },
+    {'nvim-telescope/telescope.nvim', tag = '0.1.8'},
+    {
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_enable_bold = true
+        vim.g.gruvbox_material_enable_bold = true
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    },
+    {'nvim-telescope/telescope.nvim', tag = '0.1.8'},
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function () 
+        local configs = require("nvim-treesitter.configs")
 
-				configs.setup({
-					ensure_installed = { "typescript", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-					highlight = { enable = true },
-					indent = { enable = true },  
-				})
-			end
-		}
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "gruvbox-material" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+        configs.setup({
+          ensure_installed = { "typescript", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+      end
+    }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "gruvbox-material" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })
 -- LSP Setup
 local lsp_zero = require('lsp-zero')
@@ -115,6 +115,8 @@ lsp_zero.extend_lspconfig({
 })
 require('lspconfig').lua_ls.setup({
 })
+require('lspconfig').clangd.setup({
+})
 require("typescript-tools").setup({
 })
 local cmp = require('cmp')
@@ -125,17 +127,19 @@ cmp.setup({
     {name = 'nvim_lsp'},
   },
   mapping = cmp.mapping.preset.insert({
--- Navigate between completion items
-    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-
     -- `Enter` key to confirm completion
     ['<CR>'] = cmp.mapping.confirm({select = false}),
 
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.vim_snippet_jump_forward(),
+    ['<C-b>'] = cmp_action.vim_snippet_jump_backward(),
+
     -- Scroll up and down in the completion documentation
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),  }),
   snippet = {
     expand = function(args)
       vim.snippet.expand(args.body)
