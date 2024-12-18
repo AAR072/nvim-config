9e6b391a5d2e65150f2ee27a7a2d169b4da200c1
Ali-Akber Raza <aaoblivion@gmail.com>
2024-12-18 17:52:22 -0500

    stuff

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -55,7 +55,6 @@ require("lazy").setup({
         "nvim-tree/nvim-web-devicons"
       }
     },
-    -- {"pocco81/auto-save.nvim"},
     {"tpope/vim-commentary"},
     {
       "ThePrimeagen/harpoon",
@@ -125,6 +124,43 @@ require("lazy").setup({
         })
       end
     },
+    {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+}
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -169,6 +205,7 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
+  preselect = 'item',
   sources = {
     {name = 'nvim_lsp'},
   },
